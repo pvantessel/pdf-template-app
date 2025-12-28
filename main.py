8188532c01b5fb6f965c2efb0900825fa1e758d5
pvantessel <pvantessel@gmail.com>
2025-12-28 15:01:52 +0100

    28-12-2025 First version of main with basic pdf file

---
+++ main.py
@@ -0,0 +1,15 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font(family="Helvetica", style="B", size=20)
+    pdf.set_text_color(254,0,0)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    pdf.line(x1=10,y1=20,x2=200,y2=20)
+
+pdf.output("output.pdf")
