e5fb0e101d9514e7859695170d83dd0efbcff179
pvantessel <pvantessel@gmail.com>
2025-12-30 11:35:29 +0100

    30-12-2025 Added footer

--- main.py
+++ main.py
@@ -2,17 +2,32 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
+    # Set header
     pdf.add_page()
     pdf.set_font(family="Helvetica", style="B", size=20)
     pdf.set_text_color(254,0,0)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(x1=10,y1=20,x2=200,y2=20)
 
+    # Set footer
+    pdf.ln(265)
+    pdf.set_font(family="Helvetica", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
+
     for i in range(row["Pages"] -1 ):
+        # Set header
         pdf.add_page()
+
+        # Set footer
+        pdf.ln(277) # 265 (break lines ln) + 12 (height cell header)
+        pdf.set_font(family="Helvetica", style="I", size=8)
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=row["Topic"], align="R")
 
 pdf.output("output.pdf")
